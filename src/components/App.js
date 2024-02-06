09c4008c76d94a42ecd344719b98e30bf86bf298
macdonald34 <+>
2024-02-06 11:30:49 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,21 @@
-import React, { useState } from "react";
-import ShoppingList from "./ShoppingList";
-import itemData from "../data/items";
+import React, { useState } from 'react'
+import ShoppingList from './ShoppingList'
+import itemData from '../data/items'
+import Header from './Header'
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+	const [isDarkMode, setIsDarkMode] = useState(false)
 
-  function onDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
-  }
+	function onDarkModeClick() {
+		setIsDarkMode((isDarkMode) => !isDarkMode)
+	}
 
-  return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={onDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
-      <ShoppingList items={itemData} />
-    </div>
-  );
+	return (
+		<div className={'App ' + (isDarkMode ? 'dark' : 'light')}>
+			<Header isDarkMode={isDarkMode} onDarkModeClick={onDarkModeClick}  />
+			<ShoppingList items={itemData} />
+		</div>
+	)
 }
 
-export default App;
+export default App
